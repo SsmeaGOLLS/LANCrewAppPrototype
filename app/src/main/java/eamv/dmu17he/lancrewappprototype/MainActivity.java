6492bc6fdc2f56ea4390ee2004305a661099e004
hkkrestless <32671411+hkkrestless@users.noreply.github.com>
2018-01-08 13:06:58 +0100

    Added login xml, and begun implementing login procedures

--- app/src/main/java/eamv/dmu17he/lancrewappprototype/MainActivity.java
+++ app/src/main/java/eamv/dmu17he/lancrewappprototype/MainActivity.java
@@ -2,12 +2,40 @@ package eamv.dmu17he.lancrewappprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //check if remember me and prior user info exists, if so, autocomplete loginscreen
+    }
+
+    public void loginPressed(View view)
+    {
+        EditText userName =(EditText)findViewById(R.id.userNameInput);
+        EditText password =(EditText)findViewById(R.id.passwordInput);
+
+        //check if admin
+        if(userName.getText().toString()=="admin"&&password.getText().toString()=="1234")
+        {
+            //
+        }
+        else
+        {
+            //check if username exists in database
+            //check if password is correct
+        }
+    }
+
+    public void loggedIn(userDBEntity theUser)
+    {
+        //create intent
+        //go to menuScreen
     }
 }
